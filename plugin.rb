729a75ced49e6a56729d2cc21092cdeccb8830c8
Richard <richard@communiteq.com>
2022-08-25 13:11:05 +0200

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-group-expiration
+# version: 1.0
+# author: richard@communiteq.com
+# about: Expire group memberships and send notifications about it
+# url: https://www.github.com/communiteq/discourse-group-expiration
