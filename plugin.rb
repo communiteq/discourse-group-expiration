408c62dcdbda13a4ded85219128e92fd4392d36c
Richard <richard@communiteq.com>
2022-08-25 15:36:00 +0200

    Feature: send from certain user

--- plugin.rb
+++ plugin.rb
@@ -13,18 +13,18 @@ after_initialize do
   module ::GroupExpiration
     class GroupExpiration::Utils
       def self.send_pm(username, key, nv = {})
-        system_user = Discourse.system_user
+        sending_user = User.where(username_lower: SiteSetting.group_expiration_sender.downcase).first || Discourse.system_user
         title = I18n.t("group_expiration.pm_#{key}_title", nv)
         body = I18n.t("group_expiration.pm_#{key}_body", nv)
         post = PostCreator.create!(
-          system_user,
+          sending_user,
           title: title,
           raw: body,
           archetype: Archetype.private_message,
           target_usernames: username,
           skip_validations: true
         )
-        post.topic.update_status('closed', true, system_user)
+        post.topic.update_status('closed', true, Discourse.system_user) if sending_user.id == Discourse.system_user.id
       end
     end
   end
