1dccfaecdc6b3e1f98f189d04a13cbf85093ac74
Richard <richard@communiteq.com>
2022-08-25 14:47:24 +0200

    Add utils

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,31 @@
 # author: richard@communiteq.com
 # about: Expire group memberships and send notifications about it
 # url: https://www.github.com/communiteq/discourse-group-expiration
+
+enabled_site_setting :group_expiration_enabled
+
+PLUGIN_NAME ||= "discourse-group-expiration".freeze
+
+after_initialize do
+
+  module ::GroupExpiration
+    class GroupExpiration::Utils
+      def self.send_pm(username, key, nv = {})
+        system_user = Discourse.system_user
+        title = I18n.t("group_expiration.pm_#{key}_title", nv)
+        body = I18n.t("group_expiration.pm_#{key}_body", nv)
+        post = PostCreator.create!(
+          system_user,
+          title: title,
+          raw: body,
+          archetype: Archetype.private_message,
+          target_usernames: username,
+          skip_validations: true
+        )
+        post.topic.update_status('closed', true, system_user)
+      end
+    end
+  end
+
+  require_dependency File.expand_path("../jobs/scheduled/group_expiration_checks.rb", __FILE__)
+end
