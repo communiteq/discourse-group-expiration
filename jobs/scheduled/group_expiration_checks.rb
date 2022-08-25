bba9bcb6d79b1dc7d7b8976474ddcef9d4263d93
Richard <richard@communiteq.com>
2022-08-25 14:47:34 +0200

    Fix: kill tabs

--- jobs/scheduled/group_expiration_checks.rb
+++ jobs/scheduled/group_expiration_checks.rb
@@ -7,30 +7,30 @@ module Jobs
     def execute(args)
       return unless SiteSetting.group_expiration_enabled
 
-			now = Time.now 
-			today = now.strftime('%Y-%m-%d')
-			now7 = now + 7.days
-			nextweek = now7.strftime('%Y-%m-%d')
+      now = Time.now
+      today = now.strftime('%Y-%m-%d')
+      now7 = now + 7.days
+      nextweek = now7.strftime('%Y-%m-%d')
 
-			UserCustomField.where("name like 'group_expiration_%'").each do |ucf|
-			  obj = JSON.parse(ucf['value'])
-			  status = obj['status'] || 'active'
-			  if (obj['expires'] < nextweek) && (status == 'active')
-			    # send notification for expiring
-					::GroupExpiration::Utils::send_pm(
-						ucf&.user&.username, "notice_#{obj['group'].downcase}", { 
-							groupname: obj['group'],
+      UserCustomField.where("name like 'group_expiration_%'").each do |ucf|
+        obj = JSON.parse(ucf['value'])
+        status = obj['status'] || 'active'
+        if (obj['expires'] < nextweek) && (status == 'active')
+          # send notification for expiring
+          ::GroupExpiration::Utils::send_pm(
+            ucf&.user&.username, "notice_#{obj['group'].downcase}", {
+              groupname: obj['group'],
               username: ucf&.user&.name || ucf&.user&.username,
-							date: obj['expires'],
-						}
-					)
-			    obj['status'] = 'expiring'
-			    ucf['value'] = JSON.generate(obj)
-			    ucf.save!
-			  end
+              date: obj['expires'],
+            }
+          )
+          obj['status'] = 'expiring'
+          ucf['value'] = JSON.generate(obj)
+          ucf.save!
+        end
 
-			  if (obj['expires'] < now) && (status != 'inactive')
-			    # remove user from group
+        if (obj['expires'] < now) && (status != 'inactive')
+          # remove user from group
           user = ucf.user
           group = Group.where(name: obj['group']).first
           if group.nil?
@@ -39,20 +39,20 @@ module Jobs
             group.remove(user)
           end
 
-			    # send notification for deactivated
-					::GroupExpiration::Utils::send_pm(
-						ucf&.user&.username, "expired_#{obj['group'].downcase}", { 
-							groupname: obj['group'],
+          # send notification for deactivated
+          ::GroupExpiration::Utils::send_pm(
+            ucf&.user&.username, "expired_#{obj['group'].downcase}", {
+              groupname: obj['group'],
               username: ucf&.user&.name || ucf&.user&.username,
-							date: obj['expires'],
-						}
-					)
+              date: obj['expires'],
+            }
+          )
 
-			    obj['status'] = 'inactive'
-			    ucf['value'] = JSON.generate(obj)
-			    ucf.save!
-			  end
-			end
+          obj['status'] = 'inactive'
+          ucf['value'] = JSON.generate(obj)
+          ucf.save!
+        end
+      end
     end
   end
 end
